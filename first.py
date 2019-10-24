192c6efc6a4d6ae4c801efb33b681d5cf2b403ed
Jack Vines <54452163+j-vines@users.noreply.github.com>
2019-10-24 17:28:44 -0400

    Added first.py

---
+++ first.py
@@ -0,0 +1,19 @@
+class User:
+    #User has name and id number
+    name = ""
+    uid = 0
+
+    def __init__(self, name, uid):
+        self.name = name
+        self.uid = uid
+
+    def print_user(self):
+        print("\nUser name: " + self.name)
+        print("User ID: " + str(self.uid) + "\n")
+
+#main
+name = input("Howdy!\nInput your name: ")
+user1 = User(name, 1)
+user1.print_user()
+
+
